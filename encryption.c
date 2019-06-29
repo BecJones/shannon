72a8a2aa6eee5d0a881720270585779ae18b7ae6
Becquerel Jones <becquerel.v.jones@gmail.com>
2019-06-29 17:45:00 -0400

    Output file now contains entire noise file, rather than just the part containing the signal.

--- encryption.c
+++ encryption.c
@@ -188,8 +188,9 @@ int insertSignal(struct dataString* output, struct dataString* signal, struct da
 	// Hide the signal
 	bytenum = 0;
 	bitnum = 0;
+	strcpy(output->data, noise->data);
 	for(i = offset; i < offset + signal->size; i = i + 1) {
-		noise->data[i] = (noise->data[i] & 0xFE) | ((signal->data[bytenum] >> bitnum) & 0x01);
+		output->data[i] = (output->->data[i] & 0xFE) | ((signal->data[bytenum] >> bitnum) & 0x01);
 		bitnum = bitnum + 1;
 		if(bitnum >= 7) {
 			bitnum = 0;
